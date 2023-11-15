d4685131496bc6a811581b4e82d330b75c835543
gtjones97 <gabriel897@me.com>
2023-11-15 16:35:25 -0500

    update cursor trail script on all pages

---
+++ js/cursor_trail.js
@@ -0,0 +1,21 @@
+let lastTime = 0;
+const delay = 50; // Delay in milliseconds between asterisks
+
+document.addEventListener('mousemove', function(e) {
+  const currentTime = Date.now();
+  if (currentTime - lastTime > delay) {
+    let trail = document.createElement('span');
+    trail.className = 'trail';
+    trail.textContent = '*'; // Asterisk character for fairy dust
+    document.body.appendChild(trail);
+    
+    trail.style.left = (e.pageX - 10) + 'px';
+    trail.style.top = (e.pageY - 10) + 'px';
+    
+    setTimeout(() => {
+      document.body.removeChild(trail);
+    }, 200); // Adjust time for fairy dust to disappear
+
+    lastTime = currentTime;
+  }
+});
