5e7fefaf297f223f3058ed84d39e93c68e0e98d2
gtjones97 <gabriel897@me.com>
2025-04-07 11:26:09 -0400

    improve scroll button mechanics

--- js/scrollTopButton.js
+++ js/scrollTopButton.js
@@ -1,7 +1,9 @@
 const scrollBtn = document.getElementById("scrollTopBtn");
   
       window.addEventListener("scroll", function () {
-        if (window.scrollY > 200) {
+        if (window.scrollY > 500) {
+
+            scrollBtn.style.display = "block";
           scrollBtn.style.opacity = "0.8";
           scrollBtn.style.pointerEvents = "auto";
         } else {
