7fd9d4bc17c202596d0a19fd7af0aa062b9cc622
gtjones97 <gabriel897@me.com>
2025-04-07 11:55:34 -0400

    add scroll button to every page

--- js/scrollTopButton.js
+++ js/scrollTopButton.js
@@ -1,20 +1,21 @@
 const scrollBtn = document.getElementById("scrollTopBtn");
-  
-      window.addEventListener("scroll", function () {
-        if (window.scrollY > 500) {
 
-            scrollBtn.style.display = "block";
-          scrollBtn.style.opacity = "0.8";
-          scrollBtn.style.pointerEvents = "auto";
-        } else {
-          scrollBtn.style.opacity = "0";
-          scrollBtn.style.pointerEvents = "none";
-        }
-      });
-  
-      scrollBtn.addEventListener("click", function () {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      });
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 500) {
+
+        scrollBtn.style.display = "block";
+        scrollBtn.style.opacity = "0.8";
+        scrollBtn.style.pointerEvents = "auto";
+
+    } else {
+        scrollBtn.style.opacity = "0";
+        scrollBtn.style.pointerEvents = "none";
+    }
+});
+
+scrollBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
